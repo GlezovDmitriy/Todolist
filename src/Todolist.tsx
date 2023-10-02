4c4082400598341004678730be62c7d0fb820937
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-10-02 22:37:33 +0300

    refactored code

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from "react";
+import React, {useRef, useState, KeyboardEvent, ChangeEvent} from "react";
 import {FullInput} from "./components/FullInput";
 import {FilterValuesType} from "./App";
 
@@ -25,6 +25,13 @@ export const Todolist = (props: PropsType) => {
         setNewTaskTitle('')
     }
     const isAddBtnDisabled = newTaskTitle === '' || newTaskTitle.length >= 15
+    const userMessage = newTaskTitle.length < 15
+        ? <span> Enter new title</span>
+        : <span style={{color:'red'}} > Too long message</span>
+const onKeyDownAddTask = (event: KeyboardEvent<HTMLInputElement>) => event.key ==="Enter" &&
+    onClickAddTask()
+
+ const onChangeSetNewTaskTitle = (e: ChangeEvent<HTMLInputElement>) => setNewTaskTitle(e.target.value)
     return (
         <div>
             <h3>{props.title}</h3>
@@ -38,10 +45,8 @@ export const Todolist = (props: PropsType) => {
                     }
                 }}>+</button>*/}
                 <input value={newTaskTitle}
-                       onChange={(e) => setNewTaskTitle(e.target.value)}
-                       onKeyDown={event => {
-                           event.key === 'Enter' && onClickAddTask()
-                       }}
+                       onChange={onChangeSetNewTaskTitle}
+                       onKeyDown={onKeyDownAddTask}
 
                 />
 
@@ -51,9 +56,7 @@ export const Todolist = (props: PropsType) => {
                 >+
                 </button>
                 <div>
-                    <span>{newTaskTitle.length < 15
-                        ? 'Enter new title'
-                        : 'Too long message'} </span>
+                    <span>{userMessage} </span>
                 </div>
 
             </div>
