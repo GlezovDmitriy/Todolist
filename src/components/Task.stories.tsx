b39a140bba07451a700e1f763f265872a76eeebd
GlezovDmitriy <dmitriy_glezov@mail.ru>
2024-03-04 23:14:59 +0300

    Added Task.stories.tsx

---
+++ src/components/Task.stories.tsx
@@ -0,0 +1,31 @@
+import type {Meta, StoryObj} from '@storybook/react';
+import {action} from '@storybook/addon-actions'
+import {Task} from './Task';
+import {AddItemForm} from "./AddItemForm";
+import React from "react";
+
+export  default {
+    title: 'Task Component',
+    component: Task
+}
+const changeTaskTitleCallback = action('Title changed')
+const changeTaskStatusCallback = action('Status changed')
+const removeTaskCallback = action('Title changed')
+export const TaskBaseExample = () =>{
+    return <>
+        <Task
+            t={{id:'1', isDone:true, title: 'CSS'}}
+            changeTaskTitle={changeTaskTitleCallback}
+            changeTasksStatus={changeTaskStatusCallback}
+            removeTask={removeTaskCallback}
+            todolistId={'todolistId1'}
+        />
+        <Task
+            t={{id:'2', isDone:false, title: 'JS'}}
+            changeTaskTitle={changeTaskTitleCallback}
+            changeTasksStatus={changeTaskStatusCallback}
+            removeTask={removeTaskCallback}
+            todolistId={'todolistId2'}
+        />
+    </>
+}
